635986731b60a6b6f1764f03d8e70f262fc1105b
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-10 13:29:30 -0700

    todos guest list

--- lib/presentation/providers/todos_providers.dart
+++ lib/presentation/providers/todos_providers.dart
@@ -1,15 +1,55 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_app/config/config.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../domain/domain.dart';
 
 part 'todos_providers.g.dart';
 
+const uuid = Uuid();
+
 enum FilterType { all, completed, pending }
 
-@riverpod
+@Riverpod(keepAlive: true)
 class TodoCurrentFilter extends _$TodoCurrentFilter {
   @override
   FilterType build() => FilterType.all;
 
   void setCurrentFilter(FilterType filter) {
     state = filter;
+  }
+}
+
+@Riverpod(keepAlive: true)
+class Todos extends _$Todos {
+  @override
+  List<Todo> build() => [
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: null),
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: DateTime.now()),
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: null),
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: DateTime.now()),
+        Todo(
+            id: uuid.v4(),
+            description: RandomGenerator.getRandomName(),
+            completedAt: null)
+      ];
+
+  void createTodo(String description) {
+    state = [
+      ...state,
+      Todo(id: uuid.v4(), description: description, completedAt: null)
+    ];
   }
 }
--- lib/presentation/providers/todos_providers.g.dart
+++ lib/presentation/providers/todos_providers.g.dart
@@ -6,12 +6,12 @@ part of 'todos_providers.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$todoCurrentFilterHash() => r'6802f5a78f9e70a3464eb40a0a3484158be54699';
+String _$todoCurrentFilterHash() => r'c70550f03a7d2e3287f2f2730e7615df166b1a51';
 
 /// See also [TodoCurrentFilter].
 @ProviderFor(TodoCurrentFilter)
 final todoCurrentFilterProvider =
-    AutoDisposeNotifierProvider<TodoCurrentFilter, FilterType>.internal(
+    NotifierProvider<TodoCurrentFilter, FilterType>.internal(
   TodoCurrentFilter.new,
   name: r'todoCurrentFilterProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,6 +21,20 @@ final todoCurrentFilterProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$TodoCurrentFilter = AutoDisposeNotifier<FilterType>;
+typedef _$TodoCurrentFilter = Notifier<FilterType>;
+String _$todosHash() => r'bfd41a9d74309471bd393c308218a7b56dfd163d';
+
+/// See also [Todos].
+@ProviderFor(Todos)
+final todosProvider = NotifierProvider<Todos, List<Todo>>.internal(
+  Todos.new,
+  name: r'todosProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todosHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Todos = Notifier<List<Todo>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
