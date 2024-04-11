7d5050a515f22b86fd8e55fb7eeb4fe2d43079c3
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-10 20:26:50 -0700

    stream provider

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -10,6 +11,39 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const StreamView(),
+    );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final usersInChatAsyn = ref.watch(usersInChatProvider);
+
+    if (usersInChatAsyn.isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (usersInChatAsyn.hasError) {
+      return Center(
+        child: Text(usersInChatAsyn.error.toString()),
+      );
+    }
+
+    final users = usersInChatAsyn.value!;
+
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(users[index]),
+        );
+      },
     );
   }
 }
